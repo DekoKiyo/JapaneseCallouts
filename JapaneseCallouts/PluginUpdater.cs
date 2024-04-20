a7384734b1170b531044d4570f5dbc5e21087f58
DekoKiyo <dekobokokiyomori0817@gmail.com>
2024-04-20 22:44:13 +0900

    アップデーターの基礎を追加

---
+++ JapaneseCallouts/PluginUpdater.cs
@@ -0,0 +1,17 @@
+namespace JapaneseCallouts;
+
+internal static class PluginUpdater
+{
+    private const string DATA_JSON = @"https://github.com/DekoKiyo/JapaneseCallouts/blob/main/update.json";
+    private const string DOWNLOAD_LINK = @"https://github.com/DekoKiyo/JapaneseCallouts/releases/download/{0}/JapaneseCallouts.dll";
+
+    private static readonly HttpClient Client = new();
+
+    internal static bool CheckUpdate()
+    {
+        var data = Client.GetStringAsync(DATA_JSON).Result;
+        var dic = JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
+
+        return Version.Parse(dic["latest"]) > Version.Parse(Main.PLUGIN_VERSION);
+    }
+}
