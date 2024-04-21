bbab30f7103b378c71f872895f78bd0a8253cf16
DekoKiyo <dekobokokiyomori0817@gmail.com>
2024-04-21 17:52:34 +0900

    記述漏れ

--- JapaneseCallouts/Main.cs
+++ JapaneseCallouts/Main.cs
@@ -179,7 +179,7 @@ internal class Main : Plugin
                     }
                     else
                     {
-                        var answer = Conversations.DisplayQuestionPopup(Localization.GetString("PluginUpdateAvailable"), new() { { Localization.GetString("UpdateYes"), Keys.D1 }, { Localization.GetString("UpdateNo"), Keys.D2 } }, true);
+                        var answer = Conversations.DisplayQuestionPopup(Localization.GetString("PluginUpdateAvailable", PLUGIN_NAME), new() { { Localization.GetString("UpdateYes"), Keys.D1 }, { Localization.GetString("UpdateNo"), Keys.D2 } }, true);
                         if (answer is 0)
                         {
                             PluginUpdater.Update();
