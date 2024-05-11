8e9bf8a139472c3b3293372bf7280a2ec12b95d0
DekoKiyo <dekobokokiyomori0817@gmail.com>
2024-05-11 19:23:32 +0900

    新しい銀行を追加

--- JapaneseCallouts/Callouts/BankHeist.cs
+++ JapaneseCallouts/Callouts/BankHeist.cs
@@ -8,10 +8,10 @@ internal class BankHeist : CalloutBase
     private bool Arrived = false;
     private readonly List<Ped> Robbers = [];
 
-    private static readonly Dictionary<Vector3, (List<(Vector3 pos, float heading)> positions, List<(Vector3 pos, string hash)> doors)> BankData = new()
+    private static readonly Dictionary<Vector3, (List<(Vector3 pos, float heading)> positions, List<(Vector3 pos, uint hash)> doors)> BankData = new()
     {
         {
-            new(149.0f, -1042.7f,29.3f),
+            new(149.0f, -1042.7f, 29.3f),
             (
                 [
                     (new(152.56f, -1041.58f, 30f), 8.51f),
@@ -20,7 +20,23 @@ internal class BankHeist : CalloutBase
                     (new(146.29f, -1045.20f, 30f), 60.17f),
                 ],
                 [
-
+                    (new(149.6298f, -1037.231f, 29.71915f), 3142793112),
+                    (new(152.0632f, -1038.124f, 29.71909f), 73386408),
+                ]
+            )
+        },
+        {
+            new(317.0f, -279.8f, 54.2f),
+            (
+                [
+                    (new(317.0f, -279.8f, 54.2f), 27.9f),
+                    (new(308.9f, -277.1f, 54.2f), 274.0f),
+                    (new(307.4f, -281.8f, 54.2f), 303.7f),
+                    (new(311.59f, -283.51f, 54.16f), 69.2f),
+                ],
+                [
+                    (new(313.9587f, -275.5965f, 54.51586f), 3142793112),
+                    (new(316.3925f, -276.4888f, 54.5158f), 73386408),
                 ]
             )
         }
