e3dec26bfce2a976bb98ce5d1045e14bb9c70cf7
DekoKiyo <dekobokokiyomori0817@gmail.com>
2024-08-02 22:26:41 +0900

    フォルダ変更

--- xml.ps1
+++ xml.ps1
@@ -3,13 +3,14 @@ $GrandTheftAutoV = $env:GrandTheftAutoV
 
 # GTA5側のディレクトリたち
 $PluginsLSPDFRFolder = $GrandTheftAutoV + "\plugins\LSPDFR"
-$XmlFolder = ".\Xml\"
+$XmlFolder = ".\JapaneseCallouts\Resources\"
 
 Write-Host "PowerShell $($PSVersionTable.PSEdition) Version $($PSVersionTable.PSVersion)" -ForegroundColor Cyan
 Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { Write-Error $_ -ErrorAction Continue; exit 1 }
 
 Write-Host "[Copy] In progress..." -ForegroundColor DarkCyan
 Copy-Item $XmlFolder "$($PluginsLSPDFRFolder)\JapaneseCallouts" -Recurse -Force
+Rename-Item -Path "$($PluginsLSPDFRFolder)\JapaneseCallouts\Resources" -NewName "$($PluginsLSPDFRFolder)\JapaneseCallouts\Xml" -Force
 Write-Host "[Copy] Done!" -ForegroundColor Green
 
 Write-Host "All process was successfully done!" -ForegroundColor Green
